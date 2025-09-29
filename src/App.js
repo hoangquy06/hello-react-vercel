a191bef7f6803a2489e640ea6e116ee890079723
Qúy Võ Hoàng <23521322@ms.uit.edu.vn>
2025-09-30 03:58:40 +0700

    Initial commit - Hello React

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import React from 'react';
+
+function App() {
+    return (
+        <div style={{ textAlign: 'center', marginTop: '40px' }}>
+            <h1>Hello World — React app deployed on Vercel </h1>
+            <p>Made with Create React App</p>
+        </div>
+    );
+}
+
+export default App;
